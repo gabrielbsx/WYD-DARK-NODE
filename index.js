4de276d813251a122b622662ff4b13631a72bc66
Gabriel <gabrielturing@gmail.com>
2020-08-24 21:40:17 -0300

    Merge

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 //Loading modules
 const express = require('express');
 const http = require('http');
+<<<<<<< HEAD
+=======
+
+>>>>>>> parent of fb05b5e... Implemented RoutesMgr.
 const app = express();
 
 app.set('view engine', 'ejs');
