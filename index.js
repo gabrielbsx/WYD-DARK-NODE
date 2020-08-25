e89f7d5e055a037069f29130d61f835b94ad05c9
patrikveloso <patrikveloso@gmail.com>
2020-08-24 21:27:01 -0300

    Fix routes error

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 //Loading modules
 const express = require('express');
-const http = require('http');
 const app = express();
 const routes = require('./src/Routes/RoutesMgr')
 
@@ -8,6 +7,8 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname+'/public'));
 
-http.createServer((req, res) => {
-    res.end('Initialize web server');
-}).listen(3000);
+app.use('/',routes);
+
+app.listen(3000,() => {
+   
+});
