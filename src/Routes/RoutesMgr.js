d1a959e57e92720535f2695ba92a6a572df930ce
patrikveloso <patrikveloso@gmail.com>
2020-08-24 21:24:25 -0300

    Routes mgr

--- src/Routes/RoutesMgr.js
+++ src/Routes/RoutesMgr.js
@@ -1,9 +1,15 @@
 const routes = require('express').Router();
 
+<<<<<<< Updated upstream
 routes.get('/', (req, res) => {
     res.status(200).json({
         message: 'Coonnected!'
     });
 });
+=======
+routes.get('/',(req, res)=>{
+    res.status(200).json({ message: 'Connected!' });
+})
+>>>>>>> Stashed changes
 
 module.exports = routes;
